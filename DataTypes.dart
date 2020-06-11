9fd7e761a83489150cb8ea36db060b165f545ce3
PRANAV-PANU <54212829+PRANAV-PANU@users.noreply.github.com>
2020-06-11 20:32:27 +0530

    A bit about data types

---
+++ DataTypes.dart
@@ -0,0 +1,32 @@
+void main(){
+  //Built in data types
+  //var automatically takes up the preffered value
+  //int
+  int age =20;
+  var age1 = 20;
+  print("age = $age");
+  print("age = $age1");
+  //we can use hexvalue also but make sure we have 8 char after x
+  var hexValue = 0XABCDECDE;
+  print("hexValue = $hexValue");
+  //double
+  double value= 20.0;
+  var value2 = 30.4;
+  var exponents = 1.42e2;
+ 
+  print("exponents = $exponents"); 
+  print("value = $value");
+  print("value2 = $value2");
+  
+  //String
+  String name = "Henry";
+  var name2 = "Matt";
+  print("name = $name");
+  print("name1 = $name2");
+  
+  //boolean
+  bool isValid = true;
+  bool isValid1 = false;
+  print("isValid = $isValid");
+  print("isValid1 = $isValid1");
+}
