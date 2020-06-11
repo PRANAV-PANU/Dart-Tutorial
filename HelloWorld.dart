a164270a040323de57dc17232fbd0c2019b9347f
PRANAV-PANU <54212829+PRANAV-PANU@users.noreply.github.com>
2020-06-11 20:07:25 +0530

    Added Comments

---
+++ HelloWorld.dart
@@ -0,0 +1,11 @@
+void main(){
+  /*
+   Adding Comments 
+   */
+  //printing a string
+  print("Hello World");
+  //performing arithmetic operations
+  print(12/4);
+  //Printing boolean values
+  print(true);
+}
