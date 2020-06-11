43dc6631b27cb11bbbf40f778528727f0089e05e
PRANAV-PANU <54212829+PRANAV-PANU@users.noreply.github.com>
2020-06-11 21:17:19 +0530

    Little more about Strings

---
+++ String.dart
@@ -0,0 +1,17 @@
+void main(){
+  //The value in a data type is known as literal
+  //String different ways to define
+   String name =  "pranav";
+   var str = 'good';
+   var str2 = "It's not possible";
+   //printing a string 
+   print(name);
+   //string interpolation
+   var l = 20;
+   var b = 30;
+   print("The area of Rectangle is ${l*b}");
+   print("The number of characters in pranav is ${name.length}");
+  
+  
+}
+            
