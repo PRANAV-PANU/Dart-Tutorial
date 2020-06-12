58826fc4cf4e8abf916509f99e2705fd9bd3d481
PRANAV-PANU <54212829+PRANAV-PANU@users.noreply.github.com>
2020-06-12 17:10:38 +0530

    Details about for in loop also

---
+++ ForLoop.dart
@@ -0,0 +1,17 @@
+void main(){
+// Whenever we want to print a number for eg many times we use loops
+//Gneral syntax of for loop
+// for(initializer;condition;increment/decrement)
+  
+  for(int i=0;i<10;i++){
+    print('HEllo'); 
+  }
+  
+  //List can be defined as the collection of data types
+  List planetList = ["Mercury","Venus","Earth","Mars"];
+  
+  for(String planet in planetList){
+    print(planet);
+  }
+
+}
