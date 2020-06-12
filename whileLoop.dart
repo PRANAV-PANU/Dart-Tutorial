bbc5835d0f863a452752b319b04cd8ce723641f1
PRANAV-PANU <54212829+PRANAV-PANU@users.noreply.github.com>
2020-06-12 17:31:36 +0530

    While loop

---
+++ whileLoop.dart
@@ -0,0 +1,13 @@
+void main(){
+// Whenever we want to print a number for eg many times we use loops
+//Gneral syntax of while loop
+// while(condition)
+  
+  var i=1;
+ while(i<=10){
+    print(i);
+   i++;
+  }
+  
+
+}
