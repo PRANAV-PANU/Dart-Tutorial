20c8e97a7f625117843a603d3e32ff32d12e5cc8
PRANAV-PANU <54212829+PRANAV-PANU@users.noreply.github.com>
2020-06-12 20:47:44 +0530

    Do while lop

---
+++ DoWhile.dart
@@ -0,0 +1,16 @@
+void main(){
+// Whenever we want to print a number for eg many times we use loops
+//Gneral syntax of while loop
+//do{
+//   exeutable code
+//} while(condition);
+  //do while loop run atleast once
+  
+  var i=1;
+do{
+    print(i);
+   i++;
+  }while(i<=10);
+  
+
+}
