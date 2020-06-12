d7363597f4f8b2c17b38493495972d231b4056fb
PRANAV-PANU <54212829+PRANAV-PANU@users.noreply.github.com>
2020-06-12 16:18:36 +0530

    Detail about if else ladder

---
+++ ifelse.dart
@@ -0,0 +1,36 @@
+void main(){
+  
+  //if else statement general
+  /*if(condition){
+     \\execute code
+     }else(condition){
+     //execute code
+     }
+  */
+  
+var marks= 70;
+  if(marks >60){
+    print("You are Promoted");
+  }else{
+    print("You need to wrk hard");
+  }
+  
+  //if elseif statement general
+  /*if(condition){
+     \\execute code
+     }else if(condition){
+     //execute code
+     }else(condition){
+     //execute code
+     }
+  */
+  
+var salary = 1600;
+  if(salary>20000){
+    print("You are Promoted");
+  }else if(salary>15000 && salary<20000){
+    print("Good work");
+  }else{
+    print("You need to wrk hard");
+  }
+}
